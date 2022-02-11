f83651725244340c0db08fba8d89ec2f2413101d
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2022-02-11 14:05:41 +0100

    searching for last changed file

---
+++ loop.py
@@ -0,0 +1,28 @@
+import os
+
+
+def is_file_accepted(file):
+    filename, file_extension = os.path.splitext(file)
+    return file_extension in [".ma", ".mb"]
+
+
+def find_latest(path):
+    files = filter(is_file_accepted, os.listdir(path))
+    if (len(files) > 0):
+        full_paths = map(lambda file: os.path.join(path, file), files)
+        full_paths.sort(reverse=True, key=lambda file: os.path.getmtime(file))
+        print(full_paths[0])
+
+
+def find_best(path):
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for (dir) in filter(lambda d: d in ["Animation", "Layout", "Posing"], dirnames):
+            publish_path = os.path.join(dirpath, dir, "publish")
+            if (os.path.isdir(publish_path)):
+                print("-------- " + publish_path + " --------")
+                find_latest(publish_path)
+
+
+find_best("./test-fs/scenario-1")
+find_best("./test-fs/scenario-2")
+find_best("./test-fs/scenario-3")
